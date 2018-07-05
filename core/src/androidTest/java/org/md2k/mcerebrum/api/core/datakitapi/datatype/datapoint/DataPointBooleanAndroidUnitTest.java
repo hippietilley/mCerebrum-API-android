b6b6784b65ae223efa44b60fb0b13c16cf894c8c
Xavier Tilley <hippietilley@gmail.com>
2018-07-05 16:30:09 -0500

    Removed unused import

--- core/src/androidTest/java/org/md2k/mcerebrum/api/core/datakitapi/datatype/datapoint/DataPointBooleanAndroidUnitTest.java
+++ core/src/androidTest/java/org/md2k/mcerebrum/api/core/datakitapi/datatype/datapoint/DataPointBooleanAndroidUnitTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsNot.not;
-import static org.hamcrest.core.IsSame.sameInstance;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
