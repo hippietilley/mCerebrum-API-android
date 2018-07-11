dc9405eb98a095835a4ffadf530f7619000a7f73
Xavier Tilley <hippietilley@gmail.com>
2018-07-11 14:41:35 -0500

    Created unit test for DataSourceMetaData

---
+++ core/src/androidTest/java/org/md2k/mcerebrum/api/core/datakitapi/DataSourceMetaDataAndroidUnitTest.java
@@ -0,0 +1,62 @@
+package org.md2k.mcerebrum.api.core.datakitapi;
+
+import android.os.Parcel;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Test;
+import org.md2k.mcerebrum.api.core.datakitapi.datatype.Data;
+
+import java.util.HashMap;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+@SmallTest
+public class DataSourceMetaDataAndroidUnitTest {
+    private final String testTitle = "Test Title";
+    private final String testSummary = "Test Summary";
+    private final String testDesciption = "Test Description";
+    private final String testKey = "key";
+    private final String testValue = "value";
+    private HashMap<String, String> testCustom = new HashMap<>();
+    private DataSourceMetaData testDataSourceMetaData;
+
+    @Test
+    public void DataSourceMetaDataBuilderTest() {
+        testDataSourceMetaData = new DataSourceMetaData.Builder().setValue(testKey, testValue).build();
+        assertEquals(testValue, testDataSourceMetaData.getValue(testKey));
+
+        testDataSourceMetaData = new DataSourceMetaData.Builder().setTitle(testTitle)
+                .setSummary(testSummary).setDescription(testDesciption).build();
+        assertEquals(testTitle, testDataSourceMetaData.getTitle());
+        assertEquals(testSummary, testDataSourceMetaData.getSummary());
+        assertEquals(testDesciption, testDataSourceMetaData.getDescription());
+        assertEquals(null, testDataSourceMetaData.getValue(testKey));
+    }
+
+    @Test
+    public void DataSourceMetaData_ParcelableWriteReadTest() {
+        testDataSourceMetaData = new DataSourceMetaData.Builder().setTitle(testTitle)
+                .setSummary(testSummary).setDescription(testDesciption).setValue(testKey, testValue).build();
+
+        // Write to parcel
+        Parcel parcel = Parcel.obtain();
+        testDataSourceMetaData.writeToParcel(parcel, testDataSourceMetaData.describeContents());
+
+        // After writing, reset the parcel for reading
+        parcel.setDataPosition(0);
+
+        // Read the data.
+        DataSourceMetaData createdFromParcel = DataSourceMetaData.CREATOR.createFromParcel(parcel);
+        DataSourceMetaData[] createdFromParcelArray = DataSourceMetaData.CREATOR.newArray(1);
+
+        // Verify results.
+        assertThat(createdFromParcelArray.length, is(not(0)));
+        assertEquals(testDataSourceMetaData.getTitle(), createdFromParcel.getTitle());
+        assertEquals(testDataSourceMetaData.getSummary(), createdFromParcel.getSummary());
+        assertEquals(testDataSourceMetaData.getDescription(), createdFromParcel.getDescription());
+        assertEquals(testDataSourceMetaData.getValue(testKey), createdFromParcel.getValue(testKey));
+    }
+}
