36d14b229af21909042eb3e3f5880f074fc91d58
Xavier Tilley <hippietilley@gmail.com>
2018-08-21 14:58:04 -0500

    Tests for MCPluginParam

---
+++ core/src/androidTest/java/org/md2k/mcerebrum/api/core/pluginapi/MCPluginParamAndroidUnitTest.java
@@ -0,0 +1,55 @@
+package org.md2k.mcerebrum.api.core.pluginapi;
+
+import android.os.Parcel;
+
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.md2k.mcerebrum.api.core.pluginapi.MCPluginParam;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.assertThat;
+
+public class MCPluginParamAndroidUnitTest {
+    public static String testTitle = "Test title";
+    public static String testId = "Test Id";
+    public static JSONObject testJSONObject = new JSONObject();
+    public MCPluginParam testMCPluginParam;
+
+    @Before
+    public void createPluginParam() {
+        testMCPluginParam = new MCPluginParam(testTitle, testId, testJSONObject);
+    }
+
+    @Test
+    public void objectCreationTest() {
+        assertEquals(testTitle, testMCPluginParam.getTitle());
+        assertEquals(testId, testMCPluginParam.getId());
+        assertEquals(testJSONObject, testMCPluginParam.getParams());
+    }
+
+    @Test
+    public void MCPluginParam_ParcelableWriteRead() {
+        // Write data to parcel.
+        Parcel parcel = Parcel.obtain();
+        testMCPluginParam.writeToParcel(parcel, testMCPluginParam.describeContents());
+
+
+        // After writing, reset the parcel for reading
+        parcel.setDataPosition(0);
+
+        // Read the data.
+        MCPluginParam createdFromParcel = MCPluginParam.CREATOR.createFromParcel(parcel);
+        MCPluginParam[] createdFromParcelArray = MCPluginParam.CREATOR.newArray(1);
+
+        // Verify results.
+        assertThat(createdFromParcelArray.length, is(not(0)));
+        assertEquals(testMCPluginParam.getTitle(), createdFromParcel.getTitle());
+        assertEquals(testMCPluginParam.getId(), createdFromParcel.getId());
+        assertEquals(testMCPluginParam.getParams().toString(), createdFromParcel.getParams().toString());
+    }
+}
+
